f7eaacc45a5a632bc1074fdef469ed72c6813144
Adril1n <addeboii006600@gmail.com>
2025-03-07 15:56:49 +0100

    added app.js

--- app.js
+++ app.js
@@ -239,7 +239,7 @@ app.get('/previous-workouts', (req, res) => {
     res.render('indexPreviousWorkouts');
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
